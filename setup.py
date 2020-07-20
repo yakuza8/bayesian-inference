c18736b86c513e7b024db617cc64becfd77f66f5
Berat Cankar <berat.cankar@udemy.com>
2020-07-20 21:34:49 +0300

    Add requirements to setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("requirements.txt", "r") as reqs:
+    requirements = reqs.read().splitlines()
+
 setuptools.setup(
-    name="bayesian-inference-yakuza8",
-    version="1.0.0",
+    name="bayesian-inference",
+    version="1.0.1",
     author="Berat Cankar",
     author_email="berat.cankar@gmail.com",
     description="Bayesian Inference library over network",
@@ -13,6 +16,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/yakuza8/bayesian-inference",
     packages=setuptools.find_packages(),
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
